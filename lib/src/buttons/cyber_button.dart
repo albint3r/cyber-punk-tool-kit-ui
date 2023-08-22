84d7e8cb0cddee775d2cb2cd3debdc7a9eadab86
albint3r <albint3r@gmail.com>
2023-08-22 10:08:13 -0600

    Add animations and onTap CyberButton

--- lib/src/buttons/cyber_button.dart
+++ lib/src/buttons/cyber_button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'widgets/single_cyber_container.dart';
 
-class CyberButton extends StatelessWidget {
+class CyberButton extends StatefulWidget {
   const CyberButton({
     this.child,
     this.height = 50,
@@ -12,6 +12,7 @@ class CyberButton extends StatelessWidget {
     this.primaryColorSmallContainer = Colors.white60,
     this.secondaryColorSmallContainer = Colors.white10,
     this.bottomBorder = 3,
+    this.onTap,
     super.key,
   });
 
@@ -23,57 +24,96 @@ class CyberButton extends StatelessWidget {
   final Color primaryColorSmallContainer;
   final Color secondaryColorSmallContainer;
   final double bottomBorder;
+  final void Function()? onTap;
+
+  @override
+  State<CyberButton> createState() => _CyberButtonState();
+}
+
+class _CyberButtonState extends State<CyberButton>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animationBottomFrame;
+  late final Animation<double> _animationTopFrame;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 800,
+      ),
+    );
+
+    _animationBottomFrame =
+        Tween<double>(begin: widget.width, end: 0).animate(_controller);
+    _animationTopFrame =
+        Tween<double>(begin: 0, end: widget.width).animate(_controller);
+
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: CustomDiagonalClipPath(),
-      child: Stack(
-        children: [
-          SizedBox(
-            width: width,
-            height: height,
-          ),
-          Positioned(
-            top: 5,
-            bottom: 5,
-            left: 0,
-            child: SizedBox(
-              child: SingleDiagonalContainer(
-                width: width,
-                height: height,
-                primaryColor: primaryColorBigContainer,
-                secondaryColor: secondaryColorBigContainer,
-                bottomBorder: 0,
-                child: child,
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return ClipPath(
+          clipper: CustomDiagonalClipPath(),
+          child: Stack(
+            children: [
+              SizedBox(
+                width: widget.width,
+                height: widget.height,
               ),
-            ),
+              // MAIN CONTAINER
+              Positioned(
+                top: 5,
+                bottom: 5,
+                left: 0,
+                child: InkWell(
+                  onTap: widget.onTap,
+                  child: SingleDiagonalContainer(
+                    width: widget.width,
+                    height: widget.height,
+                    primaryColor: widget.primaryColorBigContainer,
+                    secondaryColor: widget.secondaryColorBigContainer,
+                    bottomBorder: 0,
+                    child: widget.child,
+                  ),
+                ),
+              ),
+              // Bottom DETAIL CONTAINER
+              Positioned(
+                left: _animationBottomFrame.value,
+                bottom: 0,
+                child: SingleDiagonalContainer(
+                  height: widget.height / 4,
+                  width: widget.width,
+                  primaryColor: widget.primaryColorSmallContainer,
+                  secondaryColor: widget.secondaryColorSmallContainer,
+                  bottomBorder: widget.bottomBorder,
+                ),
+              ),
+              // Top DETAIL CONTAINER
+              Positioned(
+                left: _animationTopFrame.value * .50,
+                right: _animationTopFrame.value * .05,
+                top: 0,
+                child: SingleDiagonalContainer(
+                  height: widget.height / 7,
+                  width: widget.width,
+                  primaryColor: widget.primaryColorSmallContainer,
+                  secondaryColor: widget.secondaryColorSmallContainer,
+                  bottomBorder: 0,
+                ),
+              ),
+            ],
           ),
-          Positioned(
-            left: 0,
-            bottom: 0,
-            child: SingleDiagonalContainer(
-              height: height / 4,
-              width: width,
-              primaryColor: primaryColorSmallContainer,
-              secondaryColor: secondaryColorSmallContainer,
-              bottomBorder: bottomBorder,
-            ),
-          ),
-          Positioned(
-            left: width * .50,
-            right: width * .05,
-            top: 0,
-            child: SingleDiagonalContainer(
-              height: height / 7,
-              width: width,
-              primaryColor: primaryColorSmallContainer,
-              secondaryColor: secondaryColorSmallContainer,
-              bottomBorder: 0,
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
